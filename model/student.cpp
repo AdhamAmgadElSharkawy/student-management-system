ad4fecd32f5714066edede9040c79f1386e1625f
Adham El-Sharkawy <adham.elsharkawy0666@gmail.com>
2025-08-01 21:36:51 +0300

    create student model

---
+++ model/student.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class student {
+    private:
+        string name;
+        int id;
+        int age;
+        string phone_number;
+        double gpa;
+    public:
+        // setter functions
+        void set_name(string name){
+            this-> name = name;
+        }
+        void set_id(int id){
+            this-> id = id;
+        }
+        void set_age(int age){
+            this-> age = age;
+        }
+        void set_phone_number(string phone_number){
+            this-> phone_number=phone_number;
+        }
+        void set_gpa(double gpa){
+            this-> gpa=gpa;
+        }
+
+        // Getter functions
+        string get_name(){
+            return name;
+        }
+        int get_id(){
+            return id;
+        }
+        int get_age(){
+            return age;
+        }
+        string get_phone_number(){
+            return phone_number;
+        }
+        double get_gpa(double gpa){
+            return gpa;
+        }
+        
+};
