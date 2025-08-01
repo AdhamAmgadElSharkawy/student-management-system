48e538a785f854339f1b6a58147210923632bbf5
Adham El-Sharkawy <adham.elsharkawy0666@gmail.com>
2025-08-01 23:40:06 +0300

    create person file and do Inheritance between files

--- model/student.cpp
+++ model/student.cpp
@@ -1,51 +1,19 @@
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-class Student {
+#include "person.cpp"
+class Student : public Person{
     private:
-        string name;
-        int id;
-        int age;
-        string phone_number;
         double gpa;
     public:
-        Student(string name, int id, int age, string phone_number, double gpa) {
-            this->name = name;
-            this->id = id;
-            this->age = age;
-            this->phone_number = phone_number;
+        Student(string name, int id, int age, string phone_number, double gpa) 
+            : Person(name, id, age, phone_number)
+        {
             this->gpa = gpa;
         }
         // setter functions
-        void set_name(string name){
-            this-> name = name;
-        }
-        void set_id(int id){
-            this-> id = id;
-        }
-        void set_age(int age){
-            this-> age = age;
-        }
-        void set_phone_number(string phone_number){
-            this-> phone_number=phone_number;
-        }
         void set_gpa(double gpa){
             this-> gpa=gpa;
         }
 
         // Getter functions
-        string get_name(){
-            return name;
-        }
-        int get_id(){
-            return id;
-        }
-        int get_age(){
-            return age;
-        }
-        string get_phone_number(){
-            return phone_number;
-        }
         double get_gpa(){
             return gpa;
         }
--- model/teacher.cpp
+++ model/teacher.cpp
@@ -1,50 +1,18 @@
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-class Teacher {
+#include "person.cpp"
+class Teacher : public Person {
     private :
-        string name;
-        int id;
-        int age;
-        string phone_number;
         double salary;
     public :
-        Teacher(string name, int id, int age, string phone_number, double salary) {
-            this->name = name;
-            this->id = id;
-            this->age = age;
-            this->phone_number = phone_number;
+        Teacher(string name, int id, int age, string phone_number, double salary)
+           : Person(name, id, age, phone_number)
+        {
             this->salary = salary;
         }
         // setter functions
-        void set_name(string name){
-            this-> name = name;
-        }
-        void set_id(int id){
-            this-> id = id;
-        }
-        void set_age(int age){
-            this-> age = age;
-        }
-        void set_phone_number(string phone_number){
-            this-> phone_number=phone_number;
-        }
         void set_salary(double salary){
             this->salary =salary;
         }
         // Getter functions
-        string get_name(){
-            return name;
-        }
-        int get_id(){
-            return id;
-        }
-        int get_age(){
-            return age;
-        }
-        string get_phone_number(){
-            return phone_number;
-        }
         double get_salary(){
             return salary;
         }
