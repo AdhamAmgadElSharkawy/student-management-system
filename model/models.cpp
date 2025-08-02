c66142d74dde83e4884713230d512e1ee48b9d2f
Adham El-Sharkawy <adham.elsharkawy0666@gmail.com>
2025-08-02 19:45:28 +0300

    create public data model and course model

--- model/models.cpp
+++ model/models.cpp
@@ -1,22 +1,19 @@
-// person model
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-class Person {
+// public data model
+class public_data{
     private:
-        string name;
         int id;
-        int age;
-        string phone_number;
+        string name;
     public:
-        Person(){
+        // Constructors
+        public_data (){
 
         }
-        Person (string name, int id, int age, string phone_number) {
-            this->name = name;
-            this->id = id;
-            this->age = age;
-            this->phone_number = phone_number;
+        public_data (string name,int id){
+            this-> id =id;
+            this-> name = name;
         }
         // setter functions
         void set_name(string name){
@@ -25,6 +22,31 @@ class Person {
         void set_id(int id){
             this-> id = id;
         }
+        // Getter functions
+        string get_name(){
+            return name;
+        }
+        int get_id(){
+            return id;
+        }
+};
+// person model
+class Person : public public_data{
+    private:
+        int age;
+        string phone_number;
+    public:
+        // Constructors
+        Person(){
+
+        }
+        Person (string name ,int id ,int age, string phone_number) 
+            : public_data(name, id)
+        {
+            this->age = age;
+            this->phone_number = phone_number;
+        }
+        // setter functions
         void set_age(int age){
             this-> age = age;
         }
@@ -33,12 +55,6 @@ class Person {
         }
         
         // Getter functions
-        string get_name(){
-            return name;
-        }
-        int get_id(){
-            return id;
-        }
         int get_age(){
             return age;
         }
@@ -53,11 +69,12 @@ class Teacher : public Person {
         double salary;
         vector <int> student_IDs;
     public :
+        // Constructors
         Teacher() : Person() {
             salary = 0.0;
         }
         Teacher(string name, int id, int age, string phone_number, double salary)
-           : Person(name, id, age, phone_number)
+           : Person(name ,id ,age ,phone_number)
         {
             this->salary = salary;
         }
@@ -66,7 +83,7 @@ class Teacher : public Person {
             this->salary =salary;
         }
         void set_student_IDs(vector<int>&student_IDs){
-                this->student_IDs=student_IDs;
+            this->student_IDs=student_IDs;
         }
         // Getter functions
         double get_salary(){
@@ -83,13 +100,12 @@ class Student : public Person{
         double gpa;
         vector<Teacher> teachers;
     public:
-        // Default Constructor
+        // Constructors
         Student() : Person() {
             gpa = 0.0;
         }
-
         Student(string name, int id, int age, string phone_number, double gpa) 
-            : Person(name, id, age, phone_number)
+            : Person(name ,id , age,phone_number)
         {
             this->gpa = gpa;
         }
@@ -109,4 +125,18 @@ class Student : public Person{
             return teachers;
         }
         
+};
+// course model
+class Course : public public_data{
+    private :
+        int hour;
+    public:
+        // setter functions
+        void set_hour(int hour){
+            this-> hour = hour;
+        }
+        // Getter functions
+        int get_hour(){
+            return hour;
+        }
 };
