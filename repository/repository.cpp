0bf795b63c8e2c85afd629f4c4e8dfa977b7421d
Adham El-Sharkawy <adham.elsharkawy0666@gmail.com>
2025-08-03 00:27:58 +0300

    create teacher and course repository

--- repository/repository.cpp
+++ repository/repository.cpp
@@ -27,17 +27,45 @@ class Student_repository_implementation : public Student_repository{
 };
 ////////////// course repository interface
 class Course_repository{
-
+    public:
+        virtual int add_course(Course courses) = 0;
 };
 // course repository implementation
 class Course_repository_implementation : public Course_repository{
-
+    private:
+        Course courses[50];
+        int index=0;
+    public:
+        int add_course(Course course){
+            if (index == 50){
+                cout << "full capacity of courses"<<endl;
+                return 0;
+            }else{
+                courses[index]=course;
+                index++;
+                return 1;
+            }
+        }
 };
 ////////////// teacher repository interface
 class Teacher_repository{
-
+    public:
+        virtual int add_teacher(Teacher teachers) = 0;
 };
 // teacher repository implementation
 class Teacher_repository_implementation : public Teacher_repository{
-
+    private:
+        Teacher teachers[50];
+        int index=0;
+    public:
+        int add_teacher(Teacher teacher){
+            if (index == 50){
+                cout << "full capacity of courses"<<endl;
+                return 0;
+            }else{
+                teachers[index]=teacher;
+                index++;
+                return 1;
+            }
+        }
 };
