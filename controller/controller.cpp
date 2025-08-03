ca7b54a2d39d5efa8c72b15596f88d02289b5d99
Adham El-Sharkawy <adham.elsharkawy0666@gmail.com>
2025-08-03 21:20:49 +0300

    create teacher controller

--- controller/controller.cpp
+++ controller/controller.cpp
@@ -1,4 +1,5 @@
 #include "../service/service.cpp"
+// Student_controller
 class Student_controller {
     private:
         Student_service_implementation student_service;
@@ -8,11 +9,22 @@ class Student_controller {
         }
 };
 
+// Course_controller
 class Course_controller {
     private:
         Course_service_implementation course_service;
     public:
         int add_course(Course course){
             return course_service.add_course(course);
+    }
+};
+
+// Teacher_controller
+class Teacher_controller {
+    private:
+        Teacher_service_implementation teacher_service;
+    public:
+        int add_teacher(Teacher teacher){
+            return teacher_service.add_teacher(teacher);
     }
 };
