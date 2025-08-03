88a189ae92a573b14e42f0bf2bb313aae31eeebc
Adham El-Sharkawy <adham.elsharkawy0666@gmail.com>
2025-08-03 23:08:21 +0300

    create show base list and show list

--- main.cpp
+++ main.cpp
@@ -1,32 +1,41 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+void show_base_list(){
+    cout << "                          student management system                    " <<endl;
+    cout << "please choose the service that you want to do for!"<<endl;
+    cout << "                                  services                   " <<endl;
+    cout << "     1- Student"<<"                                 2- Teacher"<<endl;
+    cout << "     3- Course"<<"                                  4- Exit"<<endl;
+}
+void show_list(string thing){
+    cout <<"                                  "<<thing<<"                                    "<<endl;
+    cout << "     1- Add "<<thing<<"                          2- Remove "<<thing<<endl;
+    cout << "     3- Edit "<<thing<<"                         4- Show "<<thing<<endl;
+    cout << "                                  5-Exit                   " <<endl;
+}
 int main(){
     int process, flag =1;
     while (flag)
     {
-        cout << "                          student management system                    " <<endl;
-    cout << "please choose what do you want to do!"<<endl;
-    cout << "                                  STUDENT                   " <<endl;
-    cout << "     1- Add Student"<<"                          2- Remove Student"<<endl;
-    cout << "     3- Edit Student"<<"                         4- Show Student"<<endl;
-    cout << "                                  5-Exit                   " <<endl;
-    cin >> process;
-    if (process==1){
-        cout << "Process Add"<<endl;
-    }else if(process==2){
-        cout << "Process Remove"<<endl;
-    }
-    else if (process==3){
-        cout << "Process Edit"<<endl;
-    }else if(process==4){
-        cout << "Process Show"<<endl;
-    }else if (process==5){
-        cout << "Process Exit"<<endl;
-        flag =0;
-    }else{
-        cout << "Invalid Choose"<<endl;
-    }   
+        show_base_list();
+        cin >> process;
+        if (process==1){
+            show_list("Student");
+            flag = 0;
+        }else if(process==2){
+            show_list("Teacher");
+            flag = 0;
+        }
+        else if (process==3){
+            show_list("Course");
+            flag = 0;
+        }else if (process==4){
+            cout << "Process Exit"<<endl;
+            flag =0;
+        }else{
+            cout << "Invalid Choose"<<endl;
+        }   
     }
 
 
